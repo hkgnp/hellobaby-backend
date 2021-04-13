77c7d9d5d6f7daee7f7b45619a928146707eeb8d
hkgnp <ben@pngs.cc>
2021-04-13 13:18:48 +0800

    Changed to UploadCare

---
+++ migrations/20210412131647-add-addeby-fk-to-products.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db) {
+  return db.addColumn('products', 'user_id', {
+    type: 'int',
+    unsigned: 'true',
+    foreignKey: {
+      name: 'product_user_fk',
+      table: 'users',
+      mapping: 'id',
+      rules: {
+        onDelete: 'restrict',
+        onUpdate: 'restrict',
+      },
+    },
+  });
+};
+
+exports.down = function (db) {
+  return null;
+};
+
+exports._meta = {
+  version: 1,
+};
--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const uploadcare = require('uploadcare-widget');
 
 // import the Product, Category and Tag model
 const { Product, Category, Tag } = require('../models');
