b02bfa90ee49b05f719f03d14af8d8650dc7c23b
hkgnp <ben@pngs.cc>
2021-04-16 17:11:41 +0800

    Added remove and update routes

--- routes/api/cart.js
+++ routes/api/cart.js
@@ -21,4 +21,17 @@ router.get('/add/:user_id/:product_id', async (req, res) => {
   });
 });
 
+router.post('/update/:user_id/:product_id', async (req, res) => {
+  let cart = await new CartServices(req.params.user_id);
+  console.log(req.body.quantity);
+  console.log(cart);
+  console.log(req.params.user_id);
+  console.log(req.params.product_id);
+  let msg = await cart.updateQuantity(req.params.product_id, req.body.quantity);
+  console.log(msg);
+  res.send({
+    message: 'Quantity has been updated',
+  });
+});
+
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -35,7 +35,7 @@ router.post('/login', async (req, res) => {
         id: user.get('id'),
       },
       process.env.TOKEN_SECRET,
-      '60m'
+      '15m'
     );
 
     let refreshToken = generateAccessToken(
